f5b7e13a6d51da8a4f69835ed2e34375b1c09bb7
Daniel Kador <dkador@gmail.com>
2013-06-09 15:03:10 -0700

    support initializing from environment

--- src/main/java/io/keen/client/java/KeenClient.java
+++ src/main/java/io/keen/client/java/KeenClient.java
@@ -39,11 +39,31 @@ public class KeenClient {
         MAPPER.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
         EXECUTOR_SERVICE = Executors.newFixedThreadPool(KeenConfig.NUM_THREADS_FOR_HTTP_REQUESTS);
+
+        initialize();
     }
 
-    private enum ClientSingleton {
+    enum ClientSingleton {
         INSTANCE;
-        private KeenClient client;
+        KeenClient client;
+    }
+
+    /**
+     * Use this to attempt to initialize the client from environment variables.
+     */
+    public static void initialize() {
+        initialize(new Environment());
+    }
+
+    /**
+     * Used for tests.
+     */
+    static void initialize(Environment env) {
+        if (env.getKeenProjectId() != null) {
+            KeenClient.initialize(env.getKeenProjectId(),
+                                  env.getKeenWriteKey(),
+                                  env.getKeenReadKey());
+        }
     }
 
     /**
