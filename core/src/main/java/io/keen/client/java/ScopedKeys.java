60c6d804331507a760a4ce2256e325c7e68a1ec9
Daniel Kador <dan@keen.io>
2016-03-28 11:54:25 -0700

    fix scoped keys for new master api key length

--- core/src/main/java/io/keen/client/java/ScopedKeys.java
+++ core/src/main/java/io/keen/client/java/ScopedKeys.java
@@ -63,7 +63,11 @@ public class ScopedKeys {
      */
     public static String encrypt(String apiKey, Map<String, Object> options)
             throws ScopedKeyException {
-        return encrypt(KeenClient.client(), apiKey, options);
+        if (apiKey.length() == 64) {
+            return encrypt(KeenClient.client(), apiKey, options);
+        } else {
+            return oldEncrypt(KeenClient.client(), apiKey, options);
+        }
     }
 
     /**
@@ -76,6 +80,51 @@ public class ScopedKeys {
      * @throws ScopedKeyException an error occurred while attempting to encrypt a Scoped Key.
      */
     public static String encrypt(KeenClient client, String apiKey, Map<String, Object> options)
+            throws ScopedKeyException {
+        try {
+            // if the user doesn't give an options, just use an empty one
+            if (options == null) {
+                options = new HashMap<String, Object>();
+            }
+
+            // convert the api key from hex string to byte array
+            final byte[] apiKeyBytes = KeenUtils.hexStringToByteArray(apiKey);
+
+            // json encode the options
+            StringWriter writer = new StringWriter();
+            client.getJsonHandler().writeJson(writer, options);
+            final String jsonOptions = writer.toString();
+
+            // setup the API key as the secret
+            final SecretKey secret = new SecretKeySpec(apiKeyBytes, "AES");
+
+            // get the right AES cipher
+            final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secret);
+
+            final AlgorithmParameters params = cipher.getParameters();
+            // get a random IV for each encryption
+            final byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
+            // do the actual encryption (this also pads jsonOptions)
+            final byte[] cipherText = cipher.doFinal(jsonOptions.getBytes("UTF-8"));
+
+            // now return the hexed iv + the hexed cipher text
+            return KeenUtils.byteArrayToHexString(iv) + KeenUtils.byteArrayToHexString(cipherText);
+        } catch (Exception e) {
+            throw new ScopedKeyException("An error occurred while attempting to encrypt a Scoped Key", e);
+        }
+    }
+
+    /**
+     * Encrypts the given options with a Keen IO API Key and creates a Scoped Key.
+     *
+     * @param client  The KeenClient to use for JSON handling.
+     * @param apiKey  Your Keen IO API Key.
+     * @param options The options you want to encrypt.
+     * @return A Keen IO Scoped Key.
+     * @throws ScopedKeyException an error occurred while attempting to encrypt a Scoped Key.
+     */
+    public static String oldEncrypt(KeenClient client, String apiKey, Map<String, Object> options)
         throws ScopedKeyException {
         try {
             // if the user doesn't give an options, just use an empty one
@@ -121,7 +170,11 @@ public class ScopedKeys {
      */
     public static Map<String, Object> decrypt(String apiKey, String scopedKey)
             throws ScopedKeyException {
-        return decrypt(KeenClient.client(), apiKey, scopedKey);
+        if (apiKey.length() == 64) {
+            return decrypt(KeenClient.client(), apiKey, scopedKey);
+        } else {
+            return oldDecrypt(KeenClient.client(), apiKey, scopedKey);
+        }
     }
 
     /**
@@ -134,6 +187,51 @@ public class ScopedKeys {
      * @throws ScopedKeyException an error occurred while attempting to decrypt a Scoped Key.
      */
     public static Map<String, Object> decrypt(KeenClient client, String apiKey, String scopedKey)
+            throws ScopedKeyException {
+        try {
+            // convert the api key from hex string to byte array
+            final byte[] apiKeyBytes = KeenUtils.hexStringToByteArray(apiKey);
+
+            // grab first 16 bytes (aka 32 characters of hex) - that's the IV
+            String hexedIv = scopedKey.substring(0, 32);
+
+            // grab everything else - that's the ciphertext (aka encrypted message)
+            String hexedCipherText = scopedKey.substring(32);
+
+            // unhex the iv and ciphertext
+            byte[] iv = KeenUtils.hexStringToByteArray(hexedIv);
+            byte[] cipherText = KeenUtils.hexStringToByteArray(hexedCipherText);
+
+            // setup the API key as the secret
+            final SecretKey secret = new SecretKeySpec(apiKeyBytes, "AES");
+
+            // get the right AES cipher
+            final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+            // initialize the cipher with the right IV
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+            cipher.init(Cipher.DECRYPT_MODE, secret, ivParameterSpec);
+
+            // do the decryption
+            String plainText = new String(cipher.doFinal(cipherText), "UTF-8");
+
+            // return the JSON decoded options map
+            return client.getJsonHandler().readJson(new StringReader(plainText));
+        } catch (Exception e) {
+            throw new ScopedKeyException("An error occurred while attempting to decrypt a Scoped Key", e);
+        }
+    }
+
+    /**
+     * Decrypts the given Keen IO Scoped Key with an API Key and returns the decrypted Scoped Key Options.
+     *
+     * @param client  The KeenClient to use for JSON handling.
+     * @param apiKey    Your Keen IO API Key.
+     * @param scopedKey The Scoped Key you want to decrypt.
+     * @return The decrypted Scoped Key Options.
+     * @throws ScopedKeyException an error occurred while attempting to decrypt a Scoped Key.
+     */
+    public static Map<String, Object> oldDecrypt(KeenClient client, String apiKey, String scopedKey)
         throws ScopedKeyException {
         try {
             // pad the api key
