ce8c46de8d92a7a4a0a4c00d12498975b96e800d
Piotr Buchman <piotrek.buchman@keen.io>
2018-05-22 14:32:07 +0200

    Fixing package names

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -9,5 +9,5 @@ set -e
 if [ `uname` = 'Linux' ] && [ `uname -m` = x86_64 ]; then
   echo "Installing Linux 64-bit packages"
   sudo apt-get update -qq
-  sudo apt-get install -qq --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch
+  sudo apt-get install -qq --force-yes libgd2-xpm-dev ia32-libs:i386 ia32-libs-multiarch
 fi
